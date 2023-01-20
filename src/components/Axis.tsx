b0759b66620cca6dc07546f9c2c39d3060aa94f8
Martin Heidegger <martin.heidegger@gmail.com>
2023-01-21 08:02:26 +0900

    fix: axis

--- src/components/Axis.tsx
+++ src/components/Axis.tsx
@@ -3,7 +3,7 @@ export interface AxisProps {
 }
 export function Axis ({ value }: AxisProps): JSX.Element {
   return <svg viewBox='0 0 21 21' width="20" height="20">
-    {value !== undefined ? <line x1={0} x2={21} y1={value * 10 + 10.5} y={value * 10 + 10.5} height={20} width={20} fill='#000' /> : null}
+    {value !== undefined ? <line x1={0} x2={21} y1={value * 10 + 10.5} y2={value * 10 + 10.5} stroke='#000' /> : null}
     <rect x={.5} y={.5} width={20} height={20} fill='rgba(0,0,0,0)' stroke={value !== undefined ? 'black' : '#aaa'} />
   </svg>
 }
